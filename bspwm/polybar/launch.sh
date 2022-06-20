6882563fe900ff13b88747fbec0ac300f9c7be36
Andy Malato <andy@malato.org>
2022-06-20 11:51:09 -0400

    Added support for running additional bar when external display is attached

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -6,6 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+##############################################################################
+# Start all of our bars here, typically one for each monitor
+##############################################################################
 #(sleep 2; polybar bar) &
-(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini) &
-#(sleep 2; polybar tray) &
+#(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini) &
+(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini primary) &
+# see if we have our second monitor connected, if so launch secondary bar
+if xrandr -q | grep -w connected | grep -q HDMI-1
+then
+	(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini secondary) &
+fi
