679958bc42801723d81d610d844a15c166a379fd
Andy Malato <andy@malato.org>
2026-01-17 10:28:38 -0500

    Updates to bspwm and polybar related scripts

---
+++ bspwm/polybar/polybar-scripts/weather-icon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/.private/weather.icon.wrapper.sh
--- bspwm/polybar/polybar-scripts/weather.sh
+++ bspwm/polybar/polybar-scripts/weather.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -s https://wttr.in/ewr?format="%t+%c"
+WXINFO=$(~/.private/weather.wrapper.sh)
+
+WX=$(echo ${WXINFO} | cut -f1 -d\|)
+WXTMP=$(echo ${WXINFO} | cut -f2 -d\|)
+
+echo "${WXTMP} ${WX}" 
