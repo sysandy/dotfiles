7bc9b4aa3b76a0f7de539ad4f828fb3b5a929b0f
Andy Malato <andy@malato.org>
2026-01-19 13:10:42 -0500

    Updated polybar configuration and launch script for surface tablet

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -12,9 +12,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 #(sleep 2; polybar bar) &
 #(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini) &
 (sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini primary) &
-(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini secondary) &
-(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini tertiary) &
-(sleep 2; polybar -c /home/andym/.config/bspwm/polybar/config.ini quaternary) &
 
 # see if we have our second monitor connected, if so launch secondary bar
 #if xrandr -q | grep -w connected | grep -q HDMI-1
